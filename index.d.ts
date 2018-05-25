9220841b192c37b0e4a1e65c76536af2e77b6dad
irysius <lestersy@hotmail.com>
2018-05-25 00:52:09 -0400

    Adding hack to get around TypeScript module issue.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 declare module "@irysius/typings-util" {
     function main(typesFolder: string, ns: string, outputFile?: string): Promise<void>;
+    // https://github.com/Microsoft/TypeScript-Handbook/issues/350
+    namespace main {}
     export = main;
 }
 
